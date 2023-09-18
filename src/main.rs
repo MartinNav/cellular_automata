41cee3f7bb04d1fcbdea8066ab3290062dec4aef
MartinNav <martinnavratilu+git@gmail.com>
2023-09-18 13:32:45 +0200

    save before lecture

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use macroquad::prelude::*;
 #[macroquad::main("Game of life")]
 async fn main() {
-    //There will be 10 by 10 grid of cells
+    //There will be 20 by 20 grid of cells
     let mut state_arr:[u8;400] = [0;400];//I may be should use array of enums
+    state_arr[25]=1;
     loop {
    clear_background(WHITE);
 
@@ -16,5 +17,19 @@ for i in 0..400{
 }
 
 fn draw_cell(index:usize, state:u8){
-    draw_rectangle(26.0*((index%20) as f32), 26.0*((index/20) as f32), 25.0, 25.0, RED);
+    let cell_color = match state{
+        0=>GREEN,
+        1=>BLUE,
+        3=>RED,
+        _=>RED
+    };
+    draw_rectangle(26.0*((index%20) as f32), 26.0*((index/20) as f32), 25.0, 25.0, cell_color);
+}
+//Todo finish this function
+fn get_neigthbour_sum(state_arr:&[u8], indx:usize)->u8{
+    let mut sum:u8 =0;
+    if indx>=20{
+        sum +=state_arr[indx-20];
+    }
+    sum
 }
