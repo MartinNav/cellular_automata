bcf8382eb628e61e361156ddc1b50177095c665f
MartinNav <martinnavratilu+git@gmail.com>
2023-09-20 12:14:30 +0200

    Its working now

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,30 @@ use macroquad::prelude::*;
 async fn main() {
     //There will be 20 by 20 grid of cells
     let mut state_arr: [u8; 400] = [0; 400]; //I may be should use array of enums
+    //Set initial conditions
     state_arr[25] = 1;
+    state_arr[66] = 1;
+    state_arr[24] = 1;
+    state_arr[65] =1;
     let mut cycle_counter: u8 = 0;
     loop {
         clear_background(WHITE);
 
         //Do stuff here
+        if cycle_counter == 15 {
+            let mut s_arr = state_arr.clone();
+            for i in 0..399{
+                if get_neigthbour_sum(&state_arr, i)>=2{
+                    s_arr[i] = 1;
+                }else {
+                    s_arr[i]=0;
+                }
 
+            }
+            state_arr = s_arr;
+            cycle_counter=0;
+        }
+        cycle_counter+=1;
         //Draw
         for i in 0..400 {
             draw_cell(i, state_arr[i])
@@ -43,7 +60,7 @@ fn get_neigthbour_sum(state_arr: &[u8], indx: usize) -> u8 {
     if indx >= 20 {
         sum += state_arr[indx - 20];
     }
-    if indx < 400 {
+    if indx < 380 {
         sum += state_arr[indx + 20]
     }
     //row
