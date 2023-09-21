e28b1339d1e9903ec3ca1f0c77cbf9e584361934
MartinNav <martinnavratilu+git@gmail.com>
2023-09-21 08:52:30 +0200

    Just made it bit more readable

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ use macroquad::prelude::*;
 async fn main() {
     //There will be 20 by 20 grid of cells
     let mut state_arr: [u8; 400] = [0; 400]; //I may be should use array of enums
+
     //Set initial conditions
     state_arr[25] = 1;
     state_arr[66] = 1;
     state_arr[24] = 1;
-    state_arr[65] =1;
-    state_arr[85] =1;
-    state_arr[105] =1;
-    state_arr[104] =1;
-    state_arr[106] =1;
+    state_arr[65] = 1;
+    state_arr[85] = 1;
+    state_arr[105] = 1;
+    state_arr[104] = 1;
+    state_arr[106] = 1;
     let mut cycle_counter: u8 = 0;
     loop {
         clear_background(WHITE);
@@ -19,24 +20,23 @@ async fn main() {
         //Do stuff here
         if cycle_counter == 15 {
             let mut s_arr = state_arr.clone();
-            for i in 0..400{
-                if get_neigthbour_sum(&state_arr, i)>=2{
+            for i in 0..400 {
+                if get_neigthbour_sum(&state_arr, i) >= 2 {
                     s_arr[i] = 1;
-                }else {
-                    s_arr[i]=0;
+                } else {
+                    s_arr[i] = 0;
                 }
-
             }
             state_arr = s_arr;
-            cycle_counter=0;
+            cycle_counter = 0;
         }
-        cycle_counter+=1;
-if is_mouse_button_down(MouseButton::Left) {
-state_arr[get_muse_as_cell_index()]=1;
-}
-if is_mouse_button_down(MouseButton::Right){
-state_arr[get_muse_as_cell_index()]=0;
-}
+        cycle_counter += 1;
+        if is_mouse_button_down(MouseButton::Left) {
+            state_arr[get_muse_as_cell_index()] = 1;
+        }
+        if is_mouse_button_down(MouseButton::Right) {
+            state_arr[get_muse_as_cell_index()] = 0;
+        }
         //Draw
         for i in 0..400 {
             draw_cell(i, state_arr[i])
@@ -83,12 +83,12 @@ fn get_neigthbour_sum(state_arr: &[u8], indx: usize) -> u8 {
     sum
 }
 ///This function will find what index of array is the mouse hovering over
-fn get_muse_as_cell_index()->usize{
-   let (xf, yf) = mouse_position();
-   let (x,y)=(xf as u32, yf as u32);
-   let mut indx = (x/26)+((y/26))*20;
-   if indx >=400{
-       indx=399;
-   }
-   indx as usize
+fn get_muse_as_cell_index() -> usize {
+    let (xf, yf) = mouse_position();
+    let (x, y) = (xf as u32, yf as u32);
+    let mut indx = (x / 26) + (y / 26) * 20;
+    if indx >= 400 {
+        indx = 399;
+    }
+    indx as usize
 }
